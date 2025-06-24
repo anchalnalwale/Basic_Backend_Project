2b6eee9f7547d762a524f44c909f29aea995abe2
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-25 00:01:02 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -7,11 +7,26 @@ app.use(bodyParser.json());
 
 const Person = require('./models/person');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Welcome to my restaurant... How can I help you ?');
 })
 
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000');
+app.post('/person', (req, res) => {
+  const data = req.body;
+  const newPerson = new Person(data);
+  newPerson.save((error, savedPerson) => {
+    if (error) {
+      console.log('Error saving person:', error);
+      res.status(500).json({ error: 'Internal Server error' })
+    }
+    else {
+      console.log('Data Saved successfully');
+      res.status(200).json(savedPerson);
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
 })
 
