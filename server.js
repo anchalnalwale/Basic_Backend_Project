8074c21b59efe3f356a46b38d46bac39af2cea29
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-25 23:40:15 +0530

    New file Added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const Person = require('./models/person');
+const MenuItem = require('./models/MenuItems');
 
 app.get('/', function (req, res) {
   res.send('Welcome to my restaurant... How can I help you ?');
@@ -18,10 +19,44 @@ app.post('/person', async (req, res) => {
     const response = await newPerson.save();
     console.log('data saved');
     res.status(200).json(response);
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
-    res.status(500).json({errpr: 'Internal Server Error'});
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+app.get('/person', async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log('data fetched');
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+app.post('/menuItems', async (req, res) => {
+  try {
+    const data = req.body;
+    const newItem = new MenuItem(data);
+    const response = await newItem.save();
+    console.log('data saved');
+    res.status(200).json(response);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+app.get('/menuItems', async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    console.log('data fetched');
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 })
 
