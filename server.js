1def851efed96531b424252b5e62bc76ad50f964
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-28 21:12:10 +0530

    Added file

--- server.js
+++ server.js
@@ -5,33 +5,10 @@ const db = require('./db');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const MenuItem = require('./models/MenuItems');
-
-app.post('/menuItems', async (req, res) => {
-  try {
-    const data = req.body;
-    const newItem = new MenuItem(data);
-    const response = await newItem.save();
-    console.log('data saved');
-    res.status(200).json(response);
-  } catch(err) {
-    console.log(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-})
-
-app.get('/menuItems', async (req, res) => {
-  try {
-    const data = await MenuItem.find();
-    console.log('data fetched');
-    res.status(200).json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-})
-
 const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
+
+app.use('/menuItems',menuItemRoutes);
 app.use('/person',personRoutes);
 
 app.listen(3000, () => {
