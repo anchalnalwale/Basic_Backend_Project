dc89e358be5d478db6693c5b7eb24e7e5e9e72ed
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-30 23:53:02 +0530

    Adding changes

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const  mongoose  = require("mongoose");
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = 'mongodb+srv://nalwaleanchal:Nalwale2004@cluster0.hrrtn5r.mongodb.net/'
+
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
