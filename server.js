00abf86a0c3da386ac8831920f7b9cf8f9bc7d4f
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-05 20:27:56 +0530

    Changes for render

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 const logRequest = (req,res,next) => {
   console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
-  next();
+  // next();
 }
+
+app.use(logRequest);
 
 app.get('/',logRequest ,function(req,res) {
   res.send('Welcome to our Hotel')
