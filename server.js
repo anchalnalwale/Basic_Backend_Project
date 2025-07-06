01f94d24edf64e225065b7c47175d88c1c7ada67
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-06 23:06:54 +0530

    changes in authentication

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const Person = require('./models/person');
+const passport = require('./auth');
+// const LocalStrategy = require('passport-local').Strategy;
+// const Person = require('./models/person');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
