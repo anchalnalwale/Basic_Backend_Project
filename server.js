7efb2ed05f3b18a3bc6b078f35ddd7a95d8226b4
anchalnalwale <nalwaleanchal@gmail.com>
2025-07-08 22:43:48 +0530

    small changes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
 app.use('/menuItems',menuItemRoutes);
-app.use('/person',localAuthMiddleware,personRoutes);
+app.use('/person',personRoutes);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
